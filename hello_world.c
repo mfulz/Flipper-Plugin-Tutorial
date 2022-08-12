7b85fbd1fe2d482c8fa0ba52bdccc053026cc95d
Walter Doekes <walter+github@wjd.nu>
2022-08-12 11:03:00 +0200

    No plugin_state memleak

--- hello_world.c
+++ hello_world.c
@@ -113,6 +113,7 @@ int32_t hello_world_app() {
     view_port_free(view_port);
     furi_message_queue_free(event_queue);
     delete_mutex(&state_mutex);
+    free(plugin_state);
 
     return 0;
 }
